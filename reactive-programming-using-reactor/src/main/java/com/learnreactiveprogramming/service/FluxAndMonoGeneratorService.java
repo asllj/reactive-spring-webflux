6f288ea593c0c56ede07ae0f8427997803868336
anasofia <sofia.lanca@diconium.com>
2024-02-20 19:27:26 +0000

    Flux and Mono basics Unit Tests

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,26 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux(){
+        return Flux.fromIterable(List.of("Anna","Jonh","Sarah"))
+                .log();
+    }
+
+    public Mono<String> nameMono(){
+        return Mono.just("Alex")
+                .log();
+    }
+    public static void main(String[] args) {
+
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux().subscribe(name -> System.out.println("Flux Name is: " + name));
+        fluxAndMonoGeneratorService.nameMono().subscribe(name -> System.out.println("Mono Name is: " + name));
+
+    }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,64 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService service = new FluxAndMonoGeneratorService();
+
+    @Test
+    public void testNamesFlux_validateAllElements(){
+        //given
+
+        //when
+        var namesFlux = service.namesFlux();
+
+        //then
+        StepVerifier.create(namesFlux)
+                .expectNext("Anna","Jonh","Sarah")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testNamesFlux_expect3Elements(){
+        //given
+
+        //when
+        var namesFlux = service.namesFlux();
+
+        //then
+        StepVerifier.create(namesFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    public void testNamesFlux_validateFirstElementAndexpectMore2Elements(){
+        //given
+
+        //when
+        var namesFlux = service.namesFlux();
+
+        //then
+        StepVerifier.create(namesFlux)
+                .expectNext("Anna")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+    @Test
+    public void testNamesMono_validateElement(){
+        //given
+
+        //when
+        var nameMono = service.nameMono();
+
+        //then
+        StepVerifier.create(nameMono)
+                .expectNext("Alex")
+                .verifyComplete();
+    }
 }
