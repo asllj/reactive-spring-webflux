78596c2b976ea6e42e0b93ae61ccd0e68040760f
anasofia <sofia.lanca@diconium.com>
2024-02-21 19:39:58 +0000

    Simple Non-Blocking Restfull API Unit test Add new Service movies-info-service module

---
+++ movies-info-service/src/test/java/unit/com/reactivespring/moviesinfoservice/controller/FluxAndMonoControllerTest.java
@@ -0,0 +1,93 @@
+package com.reactivespring.moviesinfoservice.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebFluxTest(controllers = FluxAndMonoController.class)
+@AutoConfigureWebTestClient
+class FluxAndMonoControllerTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    void flux() {
+        client.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .hasSize(3)
+                .contains(1,2,3);
+    }
+
+    @Test
+    void flux__approach2() {
+        var flux = client.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(flux)
+                .expectNext(1,2,3)
+                .verifyComplete();
+    }
+
+    @Test
+    void flux__approach3() {
+        var flux = client.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .consumeWith(listEntityExchangeResult -> {
+                    var responseBody = listEntityExchangeResult.getResponseBody();
+                    assert Objects.requireNonNull(responseBody).size() == 3;
+                });
+
+    }
+
+    @Test
+    void mono() {
+        var flux = client.get()
+                .uri("/mono")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody(String.class)
+                .consumeWith(stringEntityExchangeResult -> {
+                    var responseBody = stringEntityExchangeResult.getResponseBody();
+                    assertEquals("hello-world",responseBody);
+                });
+
+    }
+
+    @Test
+    void stream() {
+        var flux = client.get()
+                .uri("/stream")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Long.class)
+                .getResponseBody();
+
+        StepVerifier.create(flux)
+                .expectNext(0L,1L,2L,3L)
+                .thenCancel()
+                .verify();
+    }
+}
